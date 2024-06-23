51ea9a8e288381bdab9f5cae12f1a714f27b82d1
Acciarino-XVII <acciarokarim@gmail.com>
2024-06-23 23:04:44 +0200

    aggiunta logo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
         <div className="container p-12 flex justify-between">
-            <span>LOGO</span>
+            <span>            <Image
+            
+            src="/images/logo.png"
+            alt="logo"
+            width={150}
+            height={100
+
+            }            
+            
+            /></span>
             <p className="text-slate-600">All Right Reserved @ karim acciaro</p>
         </div>
     </footer>
